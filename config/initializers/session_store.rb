e23a9fbe2bd06e88f2bc6fbcd74300bc8b61fb2b
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-06-13 15:01:24 -0700

    Create new Rails project with 'rails new'

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MachineLearningAsAService::Application.config.session_store :cookie_store, key: '_machine-learning-as-a-service_session'
