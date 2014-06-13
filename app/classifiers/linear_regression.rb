8803142af6b61d94d0aac2325392b434b2cda505
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-06-13 15:40:42 -0700

    Add LinearRegression class.
    * Add classifiers directory.
    * Add initialization class for LinearRegression.
    * Make data variable accessible to the class instance.

---
+++ app/classifiers/linear_regression.rb
@@ -0,0 +1,12 @@
+# Class that is responsible for the linear regression classifier.
+
+class LinearRegression
+  attr_accessor :data
+
+  #----------------------------------------------------------------------------
+
+  def initialize(data)
+    @data = data
+  end
+
+end
