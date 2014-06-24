f55d3a8a2f143707d85f5fb276ffccb6990e2375
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-06-24 11:40:15 -0700

    Migrate to add training_data_sets.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140624182916) do
+ActiveRecord::Schema.define(version: 20140624183801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,5 +41,11 @@ ActiveRecord::Schema.define(version: 20140624182916) do
 
   add_index "developers", ["email"], name: "index_developers_on_email", unique: true, using: :btree
   add_index "developers", ["reset_password_token"], name: "index_developers_on_reset_password_token", unique: true, using: :btree
+
+  create_table "training_data_sets", force: true do |t|
+    t.integer "developer_id",      null: false
+    t.text    "raw_content",       null: false
+    t.text    "processed_content"
+  end
 
 end
