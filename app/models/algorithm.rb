7f4ac3323a2ae5ef1c13f057f9267054728b6834
Dmitri Skjorshammer <dmitriskj@gmail.com>
2014-06-24 12:01:46 -0700

    Add validations at model layer.
    * Add validations to TrainingDataSet.
    * Add validation to Algorithm.

--- app/models/algorithm.rb
+++ app/models/algorithm.rb
@@ -1,5 +1,17 @@
 class Algorithm < ActiveRecord::Base
+
+  #----------------------------------------------------------------------------
+
+  validates :name,              :presence => true
+  validates :description,       :presence => true
+  validates :string_identifier, :presence => true
+
+  #----------------------------------------------------------------------------
+
   module Types
     LINEAR_REGRESSION = "simple-linear-regression"
   end
+
+  #----------------------------------------------------------------------------
+
 end
